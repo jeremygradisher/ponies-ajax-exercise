00678099cfa5b4867443e92e97940f983d812f79
Ubuntu <ubuntu@ip-172-31-24-69.ec2.internal>
2020-11-30 21:19:48 +0000

    Added ponies model - all working

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20201130211229) do
+
+  create_table "ponies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "profession"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
