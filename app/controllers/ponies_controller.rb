18810eda08d4a702ae28fbba5d18e037cc09c518
Ubuntu <ubuntu@ip-172-31-24-69.ec2.internal>
2020-11-30 21:27:04 +0000

    Working basic ajax tutorial

--- app/controllers/ponies_controller.rb
+++ app/controllers/ponies_controller.rb
@@ -58,6 +58,8 @@ class PoniesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to ponies_url, notice: 'Pony was successfully destroyed.' }
       format.json { head :no_content }
+      #added for ajax stuff
+      format.js   { render :layout => false }
     end
   end
 
